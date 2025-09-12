95d755577f3ee8f22b3f9276b1cdfe4fc388a681
Nerorrlex911 <nerorrlex911@outlook.com>
2025-09-12 19:15:57 +0800

    UpdateManager, Base Attribute for entities

---
+++ src/main/kotlin/com/github/zimablue/attrsystem/internal/feature/attribute/BaseAttributePlayer.kt
@@ -0,0 +1,50 @@
+package com.github.zimablue.attrsystem.internal.feature.attribute
+
+import com.github.zimablue.attrsystem.AttributeSystem
+import com.github.zimablue.attrsystem.api.AttrAPI.read
+import com.github.zimablue.attrsystem.api.compiled.CompiledAttrDataCompound
+import com.github.zimablue.attrsystem.api.compiled.sub.ComplexCompiledData
+import com.github.zimablue.attrsystem.internal.manager.ASConfig
+import com.github.zimablue.devoutserver.plugin.lifecycle.Awake
+import com.github.zimablue.devoutserver.plugin.lifecycle.PluginLifeCycle
+import taboolib.common.util.asList
+import taboolib.module.configuration.util.asMap
+
+object BaseAttributePlayer {
+
+    val type
+        get() = ASConfig.options.getString("base-attribute-player.type")?.lowercase() ?: "strings"
+    val attrData
+        get() = ASConfig.options["base-attribute-player.attributes"]
+    val conditions
+        get() = ASConfig.options["base-attribute-player.conditions"]
+
+    private const val KEY = "BASIC-ATTRIBUTE"
+
+    var baseData: ComplexCompiledData = ComplexCompiledData()
+
+
+    @Awake(PluginLifeCycle.ENABLE)
+    fun onEnable() {
+        onReload()
+    }
+
+    @Awake(PluginLifeCycle.RELOAD)
+    fun onReload() {
+        val base = when (type) {
+            "nbt" -> {
+                val attrData = attrData.asMap().entries.associate { it.key to it.value!! }.toMutableMap()
+                val conditions = conditions as? List<Any>? ?: emptyList()
+                AttributeSystem.readManager.readMap(attrData, conditions)
+            }
+
+            else -> attrData?.asList()?.read()
+        }
+        baseData.base = base
+    }
+
+    fun CompiledAttrDataCompound.basePlayer(): CompiledAttrDataCompound {
+        this[KEY] = baseData
+        return this
+    }
+}
--- src/main/kotlin/com/github/zimablue/attrsystem/internal/manager/UpdateManager.kt
+++ src/main/kotlin/com/github/zimablue/attrsystem/internal/manager/UpdateManager.kt
@@ -3,22 +3,22 @@ package com.github.zimablue.attrsystem.internal.manager
 import com.github.zimablue.attrsystem.AttributeSystem
 import com.github.zimablue.attrsystem.AttributeSystem.attributeDataManager
 import com.github.zimablue.attrsystem.AttributeSystem.attributeSystemAPI
-import com.github.zimablue.attrsystem.api.AttrAPI.update
 import com.github.zimablue.attrsystem.internal.core.schedule.TaskScheduler
 import com.github.zimablue.attrsystem.internal.manager.AttributeSystemAPIImpl.remove
+import com.github.zimablue.attrsystem.utils.isAlive
 import com.github.zimablue.attrsystem.utils.validEntity
 import com.github.zimablue.devoutserver.plugin.lifecycle.Awake
 import com.github.zimablue.devoutserver.plugin.lifecycle.AwakePriority
 import com.github.zimablue.devoutserver.plugin.lifecycle.PluginLifeCycle
-import net.minestom.server.MinecraftServer
 import net.minestom.server.entity.LivingEntity
 import net.minestom.server.entity.Player
 import net.minestom.server.event.entity.EntityDeathEvent
 import net.minestom.server.event.entity.EntityDespawnEvent
+import net.minestom.server.event.entity.EntitySpawnEvent
+import net.minestom.server.event.entity.EntityTickEvent
 import net.minestom.server.event.inventory.InventoryItemChangeEvent
 import net.minestom.server.event.player.*
 import net.minestom.server.inventory.PlayerInventory
-import net.minestom.server.timer.TaskSchedule
 import taboolib.common5.Baffle
 import taboolib.common5.clong
 import java.util.concurrent.TimeUnit
@@ -38,6 +38,8 @@ object UpdateManager {
             "UpdateManager",
             50 * updatePeriod.clong
         ) {
+            // 扫描所有已缓存的实体，移除无效实体的数据(一般情况下不会出现无效数据，但以防万一)
+            // scanning all cached entities and removing invalid entity data (generally invalid data will not appear, but just in case)
             for (uuid in attributeDataManager.keys) {
                 val entity = uuid.validEntity()
                 if (entity == null || !entity.isActive || entity.isDead) {
@@ -45,10 +47,17 @@ object UpdateManager {
                         remove(uuid)
                     continue
                 }
-                attributeSystemAPI.update(entity)
             }
         }
         with(AttributeSystem.asEventNode) {
+            addListener(EntityTickEvent::class.java) { event ->
+                val entity = event.entity
+                if(!entity.isAlive()) return@addListener
+                if(entity.aliveTicks%updatePeriod == 0L) attributeSystemAPI.update(entity as LivingEntity)
+            }
+            addListener(EntitySpawnEvent::class.java) { event ->
+                if(event.entity.isAlive()) attributeSystemAPI.update(event.entity as LivingEntity)
+            }
             addListener(EntityDespawnEvent::class.java) { event ->
                 attributeSystemAPI.remove(event.entity.uuid)
             }
@@ -56,13 +65,13 @@ object UpdateManager {
                 if(event.entity !is Player) attributeSystemAPI.remove(event.entity.uuid)
             }
             addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
-                attributeSystemAPI.update(event.player)
+                event.player.updateNextTick()
             }
             addListener(PlayerRespawnEvent::class.java) {event ->
-                event.player.updateSync(1)
+                event.player.updateNextTick()
             }
             addListener(PlayerSpawnEvent::class.java) { event ->
-                event.player.updateSync(1)
+                event.player.updateNextTick()
             }
             addListener(PlayerDisconnectEvent::class.java) { event ->
                 attributeSystemAPI.remove(event.player.uuid)
@@ -73,21 +82,19 @@ object UpdateManager {
                 val inventory = event.inventory
                 if(inventory is PlayerInventory) {
                     for (player in inventory.viewers) {
-                        player.updateSync(1)
+                        player.updateNextTick()
                     }
                 }
             }
         }
     }
 
-    internal fun LivingEntity.updateSync(delay: Int = 0) {
+    internal fun LivingEntity.updateNextTick() {
         if (baffle.hasNext(uuid.toString())) {
-            MinecraftServer.getSchedulerManager().buildTask {
-                attributeSystemAPI.update(this)
-            }.delay(TaskSchedule.tick(delay)).schedule()
+            this.scheduleNextTick { attributeSystemAPI.update(it as LivingEntity) }
         }
     }
-
+    @Awake(PluginLifeCycle.RELOAD,AwakePriority.HIGH)
     fun onReload() {
         baffle.resetAll()
         baffle = Baffle.of(updateBaffle, TimeUnit.MILLISECONDS)
